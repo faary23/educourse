c5e4e104d80685ae69cf12075fe5ec2a5406165b
nindria <netania.indria@gmail.com>
2024-06-19 14:05:18 +0700

    meta email .2

--- resources/views/searchresults.blade.php
+++ resources/views/searchresults.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="dicoding:email" content="octaviadamai23@gmail.com">
     <meta name="dicoding:email" content="netania.indria@gmail.com">
     <meta name="dicoding:email" content="faryotaa@gmail.com">
     <title>Search Results</title>
