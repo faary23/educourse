3aa5f250d1dace48a164a9fd0c072e71be72a386
nindria <netania.indria@gmail.com>
2024-06-19 13:46:17 +0700

    meta html

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="dicoding:email" content="netania.indria@gmail.com">
     <title>Educourse</title>
     <!-- Tambahkan link ke Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
--- resources/views/searchresults.blade.php
+++ resources/views/searchresults.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="dicoding:email" content="netania.indria@gmail.com">
     <title>Search Results</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/searchresults.css') }}" />
